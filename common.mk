d12cb92bb27ab83c7f824146566517a18fdb36aa
Trần Nguyễn Tuấn Anh <43093213+dopaemon@users.noreply.github.com>
2025-10-04 00:54:39 +0700

    sm8550-common: Sign Rom

--- common.mk
+++ common.mk
@@ -10,6 +10,9 @@ $(call inherit-product, hardware/qcom-caf/common/common.mk)
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
+# Sign
+$(call inherit-product, vendor/lineage-priv/keys/keys.mk)
+
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
